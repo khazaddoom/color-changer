196b80b67871601ee90ca7acf8f1348bb50b5a83
g9sund <ganesh.sundara@acegroup.com>
2018-06-21 06:02:12 +0100

    Learning JS

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 
-var colorValue = '#FF1'
+// var colorValue = '#FF1'
 
-document.getElementById('color-btn').addEventListener('click', function() {
+// document.getElementById('color-btn').addEventListener('click', function() {
 
 
-   var divTarget = document.getElementsByClassName('contentArea');
+//    var divTarget = document.getElementsByClassName('contentArea');
    
-   divTarget[0].style.background = colorValue++;
+//    divTarget[0].style.background = colorValue++;
 
 
 
-});
+// });
+
+for(var i = 0; i < 10; i++) {
+    console.log(i);
+    setTimeout(function() {
+      console.log('The number is ' + i);
+    }, 1000);
+}
